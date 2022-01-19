e3f30b138d886764cebcee7043e8aa7a1ac0a45a
David Dureau <david.dureau@cea.fr>
2022-01-19 11:58:41 +0100

    [msgpass] Eclatement du fichier VarSync.cc en Sync* et GlobalSynchronize*.cc

--- src/msgpass/VarSyncMng.h
+++ src/msgpass/VarSyncMng.h
@@ -5,177 +5,10 @@
 #include <arcane/IMesh.h>
 #include <arcane/utils/MultiArray2.h>
 
+#include "msgpass/SyncItems.h"
+#include "msgpass/SyncBuffers.h"
+
 using namespace Arcane;
-
-/*---------------------------------------------------------------------------*/
-/* Encapsule la liste des items à envoyer/recevoir */
-/*---------------------------------------------------------------------------*/
-template<typename ItemType>
-class SyncItems {
- public:
-  // Le type de groupe d'items associé à ItemType
-  using ItemGroupType = ItemGroupT<ItemType>;
- public:
-  SyncItems(IMesh* mesh, Int32ConstArrayView neigh_ranks);
-  virtual ~SyncItems() {}
-
-  auto nbOwnedItemIdxPn() const {
-    return m_nb_owned_item_pn.constView();
-  }
-
-  auto nbGhostItemIdxPn() const {
-    return m_nb_ghost_item_pn.constView();
-  }
-
-  auto ownedItemIdxPn() const {
-    return ConstMultiArray2View<Integer>(m_buf_owned_item_idx.constView(),
-        m_indexes_owned_item_pn.constView(), m_nb_owned_item_pn.constView());
-  }
-
-  auto ghostItemIdxPn() const {
-    return ConstMultiArray2View<Integer>(m_buf_ghost_item_idx.constView(),
-        m_indexes_ghost_item_pn.constView(), m_nb_ghost_item_pn.constView());
-  }
-
-  // Items intérieurs qui n'interviennent pas dans les comms
-  auto privateItems() const {
-    return m_private_items;
-  }
-
-  // Items intérieurs dont les valeurs vont être envoyées
-  auto sharedItems() const {
-    return m_shared_items;
-  }
-
-  // Items fantômes dont on va recevoir des valeurs
-  auto ghostItems() const {
-    return m_ghost_items;
-  }
-
- protected:
-  // "shared" ou "owned" : les items intérieurs au sous-domaine et qui doivent être envoyés
-  // "ghost" : les items fantômes pour lesquels on va recevoir des informations
-  // _pn : _per_neigh, info par voisin
-  
-  // Nb d'items par voisin et listes des indexes de ces items
-  // Owned ou Shared
-  IntegerUniqueArray m_buf_owned_item_idx;
-  IntegerUniqueArray m_indexes_owned_item_pn;
-  IntegerUniqueArray m_nb_owned_item_pn;
-
-  // Ghost
-  IntegerUniqueArray m_buf_ghost_item_idx;
-  IntegerUniqueArray m_indexes_ghost_item_pn;
-  IntegerUniqueArray m_nb_ghost_item_pn;
-
-  // Les groupes d'items
-  // own() = private + shared  , private \inter shared = 0
-  // all() = own() + ghost
-  ItemGroupType m_private_items;
-  ItemGroupType m_shared_items;
-  ItemGroupType m_ghost_items;
-};
-
-/*---------------------------------------------------------------------------*/
-/* Emplacement de la mémoire                                                 */
-/*---------------------------------------------------------------------------*/
-enum eLocMem {
-  LM_HostMem=0,
-  LM_DevMem,
-  MAX_LocMem
-};
-
-/*---------------------------------------------------------------------------*/
-/* Encapsule les buffers des valeurs à envoyer/recevoir                      */
-/*---------------------------------------------------------------------------*/
-class MultiBufView {
- public:
-  MultiBufView();
-  MultiBufView(ArrayView<Byte*> ptrs, Int64ConstArrayView sizes, eLocMem loc_mem=LM_HostMem);
-  MultiBufView(const MultiBufView& rhs);
-
-  MultiBufView& operator=(const MultiBufView& rhs);
-
-  //! Convertit un buffer d'octets en buffer de DataType
-  template<typename DataType>
-  static ArrayView<DataType> valBuf(ArrayView<Byte> buf);
-
-  //! Convertit un buffer d'octets en buffer 2D de DataType dont la taille dans la deuxième dimension est dim2_size
-  template<typename DataType>
-  static Array2View<DataType> valBuf2(ArrayView<Byte> buf, Integer dim2_size);
-
-  //! Accès en lecture/écriture au i-ème buffer d'octets
-  ArrayView<Byte> byteBuf(Integer i);
-
-  //! Retourne [beg_ptr, end_ptr[ qui contient tous les buffers (peut-être espacés de trous)
-  Span<Byte> rangeSpan();
-
-  //! Emplacement de la mémoire
-  eLocMem& locMem() { return m_loc_mem; }
-  eLocMem locMem() const { return m_loc_mem; }
-
- protected:
-  SharedArray<Byte*> m_ptrs;
-  SharedArray<Int64> m_sizes;
-  eLocMem m_loc_mem=LM_HostMem;  //! emplacement de la mémoire
-};
-
-/*---------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------*/
-class SyncBuffers {
- public:
-  SyncBuffers(bool is_acc_avl);
-  virtual ~SyncBuffers();
-
-  //
-  void resetBuf();
-
-  // 
-  template<typename DataType>
-  void addEstimatedMaxSz(ConstMultiArray2View<Integer> item_idx_pn, Integer degree);
-
-  // 
-  void allocIfNeeded();
-
-  /*!
-   * \brief A partir des nb d'items à communiquer, estime une borne sup de la taille du buffer en octets
-   */
-  template<typename DataType>
-  Int64 estimatedMaxBufSz(IntegerConstArrayView item_sizes, Integer degree);
-
-  /*!
-   * \brief A partir de la vue sur m_buf_bytes, construit une vue par voisin des buffers
-   */
-  template<typename DataType>
-  MultiBufView multiBufView(
-    ConstMultiArray2View<Integer> item_idx_pn, Integer degree, Integer imem);
-
- protected:
-  /*!
-   * \brief A partir de la vue sur un buffer déjà alloué, construit une vue par voisin des buffers
-   */
-  template<typename DataType>
-  MultiBufView _multiBufView(
-      IntegerConstArrayView item_sizes, Integer degree,
-      Span<Byte> buf_bytes);
-
- protected:
-  struct BufMem {
-    Byte* m_ptr=nullptr;  //! Adresse de base du buffer
-    Int64 m_size=0;  //! Taille totale (en octets) du buffer
-    Int64 m_first_av_pos=0;  //! Première position disponible
-    eLocMem m_loc_mem=LM_HostMem;  //! Emplacement de la mémoire
-    void reallocIfNeededOnHost(Int64 wanted_size, bool is_acc_avl);
-#ifdef ARCANE_COMPILING_CUDA
-    void reallocIfNeededOnDevice(Int64 wanted_size);
-#endif
-  };
- protected:
-  bool m_is_accelerator_available=false;  //! Vrai si un GPU est disponible pour les calculs
-  Int64 m_buf_estim_sz=0;  //! Taille qui va servir à allouer
-  // Pour gérer les buffers sur l'hote et le device
-  BufMem m_buf_mem[2];
-};
 
 /*---------------------------------------------------------------------------*/
 /* Gère les synchronisations des mailles fantômes par Message Passing        */
@@ -225,6 +58,26 @@ class VarSyncMng {
 
   MultiAsyncRunQueue* m_neigh_queues=nullptr;  //! Pour gérer plusieurs queues pour les voisins
 };
+
+/*---------------------------------------------------------------------------*/
+/* Spécialisations pour retourner le nb de fois un type élémentaire DataType */
+/* est répété pour un représenter ItemType                                   */
+/*---------------------------------------------------------------------------*/
+template<typename ItemType, typename DataType, template<typename, typename> class MeshVarRefT>
+Integer get_var_degree(MeshVarRefT<ItemType, DataType> var) {
+  // Ne devrait jamais être appelé
+  ARCANE_ASSERT(false, ("get_var_degree à spécifialiser"));
+  return 0;
+}
+template<typename ItemType, typename DataType>
+Integer get_var_degree(MeshVariableScalarRefT<ItemType, DataType> var) {
+  return 1;
+}
+
+template<typename ItemType, typename DataType>
+Integer get_var_degree(MeshVariableArrayRefT<ItemType, DataType> var) {
+  return var.arraySize();
+}
 
 #endif
 
